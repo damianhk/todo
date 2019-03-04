541634183a39ef401e2956638f19ae685bb69674
damianhk <dhnatyk@gmail.com>
2019-03-04 14:43:30 +0100

    each category site

--- todo/urls.py
+++ todo/urls.py
@@ -14,10 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 import todolist.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', todolist.views.index, name="TodoList")
+    path('', todolist.views.index, name="TodoList"),
+    path('list/', include('todolist.urls')),
 ]
--- todolist/views.py
+++ todolist/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import TodoList, Category
 
-def index(request):
-    todos = TodoList.objects.all()
+def index(request, todos=TodoList.objects.all()):
     categories = Category.objects.all()
     if request.method == "POST":
         if "taskAdd" in request.POST:
@@ -11,17 +10,14 @@ def index(request):
             category = request.POST["category"]
             Todo = TodoList(title=title, due_date=due_date, category=Category.objects.get(name=category))
             Todo.save()
-            return redirect("/")
         if "taskDelete" in request.POST:
             checkedlist = request.POST.getlist("checkedbox")
             print('Checked list ', checkedlist)
             for item in checkedlist:
                 task = TodoList.objects.get(id=int(item))
                 task.delete()
-        if "showCategory" in request.POST:
-            print("show")
-            category = str(request.POST["showCategory"])
-            todos = TodoList.objects.filter(category=Category.objects.get(name=category))
-
-
     return render(request, "index.html", {"todos":todos, "categories":categories})
+
+def category(request, category_id):
+    todos = TodoList.objects.filter(category=Category.objects.get(name=category_id))
+    return index(request, todos)
