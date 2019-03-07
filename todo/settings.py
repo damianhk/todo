cdcd9fdc24993006d010cea070bb024a6c143a71
damianhk <dhnatyk@gmail.com>
2019-03-07 17:59:08 +0100

    secret key env

--- todo/settings.py
+++ todo/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'apy2-wnk-#h4kb!kcxmpjiaxyk5gz1!u1q^!j!1qxmls5o$$4a'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
